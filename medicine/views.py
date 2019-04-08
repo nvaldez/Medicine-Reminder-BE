e24baca54beb08bba6251c8ec71adff858ad533c
Neftali Valdez Calderon <neftalivaldez1@gmail.com>
2019-04-08 12:12:20 -0400

    created the views

--- medicine/views.py
+++ medicine/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import UserSerializer, MedicationSerializer
+from .models import User, Medication
 
-# Create your views here.
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class MedicationList(generics.ListCreateAPIView):
+    queryset = Medication.objects.all()
+    serializer_class = MedicationSerializer
+
+class MedicationDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Medication.objects.all()
+    serializer_class =MedicationSerializer
