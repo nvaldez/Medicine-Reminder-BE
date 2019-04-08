b3fa6e362a1dd0b2fa941a1099416f64e5a2fcdf
Neftali Valdez Calderon <neftalivaldez1@gmail.com>
2019-04-08 10:54:39 -0400

    Created basic model

--- medicine/models.py
+++ medicine/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.first_name
+
+class Medication(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='medications')
+    name = models.CharField(max_length=100)
+    directions = models.TextField()
+    servings = models.IntegerField()
+    refill_left = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
